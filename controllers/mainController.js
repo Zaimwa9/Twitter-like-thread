8059fe1142b94915da3a96ad535063638449234c
Zaimwa9 <B00549848@essec.edu>
2017-07-30 15:21:08 +0200

    hashtags + delete + like/unlike with ip

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,12 +2,29 @@ var Threads = require('../models/threads.js');
 var bodyParser = require('body-parser');
 var moment = require('moment');
 
+var getHashTags = function(text){
+    var hashTags, i, len, word, words;
+    words = text.split(/[\s\r\n]+/);
+    hashTags = [];
+        for (i = 0, len = words.length; i < len; i++) {
+            word = words[i];
+            if (word.indexOf('#') === 0) {
+            hashTags.push(word);
+            }
+        }
+    console.log(hashTags + ' From function')    
+    return hashTags;
+}
+
 module.exports = function(app) {
 
     // Get method when displaying comments
     app.get('/', function(req, res) {
-        console.log('received mainpage')
-        Threads.find({}, {} ,{sort: {created_at: -1}}, function (err, db_results, count){
+        console.log(req.connection.remoteAddress)
+        Threads.find({})
+         .sort({created_at: -1})
+         .limit(10).
+         exec(function (err, db_results, count){
             if(err) {console.log(err);return err};
             if(!db_results) 
             {   
@@ -15,7 +32,7 @@ module.exports = function(app) {
                 return res.render('mainPage.ejs', {comments:[]});
             }
             console.log(db_results.length + ' comments found');
-            res.render('mainPage.ejs', {comments: db_results, moment: moment})
+            res.render('mainPage.ejs', {comments: db_results, moment: moment, ip: req.connection.remoteAddress})
         })
     })
 
@@ -34,27 +51,57 @@ module.exports = function(app) {
         
         var newCom = new Object ({
             username: 'Tester' + Math.random().toFixed(2),
-            content: req.body.comment
+            content: req.body.comment,
+            ip: req.connection.remoteAddress, 
+            hashtags: getHashTags
         })
+        console.log(newCom.hashtags)
+        // Wrap it inside hashtags stuff
+
         Threads.newMessage(newCom, function(err, result){
-            if(err) return res.send(err);
-            res.redirect('/')
-        })
+        if(err) return res.send(err);
+        res.redirect('/')
+         })
     })
     // Post method when liking
 
     app.post('/addLike/:comId', function(req, res){
-        Threads.newLike(req.params.comId, function(err, cb){
+        Threads.findOne({_id: req.params.comId}, function(err, db_result){
+            if (err) return err;
+            db_result.newLike(req.connection.remoteAddress, function(err, cb){
+                if (err) return err;
+                console.log('new like');
+                res.redirect('/')                
+            })
+        })
+        /*Threads.newLike(req.params.comId, req.connection.remoteAddress, function(err, cb){
             if(err) return err;
             console.log('new like');
             res.redirect('/')
-        })
+        })*/
     })
     // Post method when removing like
     app.post('/removeLike/:comId', function(req, res){
-        Threads.removeLike(req.params.comId, function(err, cb){
+        Threads.findOne({_id: req.params.comId}, function(err, db_result){
+            if (err) return err;
+            db_result.removeLike(req.connection.remoteAddress, function(err, cb){
+                if (err) return err;
+                console.log('like removed');
+                res.redirect('/')                
+            })        
+        
+        /*Threads.removeLike(req.params.comId, req.connection.remoteAddress, function(err, cb){
             if(err) return err;
             console.log('like removed');
+            res.redirect('/')
+        })*/
+        })
+    })
+
+    app.post('/delete/:comId', function(req, res){
+        Threads.findOneAndRemove({_id: req.params.comId}, function(err, db) {
+            if (err) return err;
+            console.log('removed');
             res.redirect('/')
         })
     })
--- models/threads.js
+++ models/threads.js
@@ -5,41 +5,52 @@ var Schema = mongoose.Schema;
 
 var ThreadsSchema = new Schema({
     username: { type: String, required: true },
+    ip: String,
     content: String,
     likesArray: Array,
+    hashtags: Array,
     created_at: Date
 });
 
 ThreadsSchema.statics.newMessage = function(comment, cb) {
     console.log('Adding new comment: ' + comment.content);
-
+    
     var newMessage = new Threads ({
         username: comment.username,
+        ip: comment.ip,
         content: comment.content,
         likesArray: [],
+        hashtags: comment.hashtags(comment.content),
         created_at: new Date()
     })
 
-    newMessage.save(function(err, db_result){
+    newMessage.save(function(err, db_result) {
         if (err) console.log(err);
         console.log(db_result);
         return cb(null, db_result)
     })
 }
 
-ThreadsSchema.statics.newLike = function (comment, cb) {
-    console.log(comment)
-    Threads.findOneAndUpdate({_id: comment}, {$push: {likesArray: "x"}}, function (err, db_result) {
+ThreadsSchema.methods.newLike = function (ip, cb) {
+    this.likesArray.push(ip);
+    this.save(cb) 
+    /*console.log(comment)
+    Threads.findOneAndUpdate({_id: comment}, {$push: {likesArray: ip}}, function (err, db_result) {
         if (err) return ('Like failed')
         return cb(null, db_result)
-    })
+    })*/
 }
 
-ThreadsSchema.statics.removeLike = function (comment, cb) {
-    Threads.findOneAndUpdate({_id: comment}, {$pop: {likesArray: "x"}}, function (err, db_result) {
-        if (err) return ('Like failed')
+ThreadsSchema.methods.removeLike = function (ip, cb) {
+    var index = this.likesArray.indexOf(ip);
+    if (index > -1) {
+        this.likesArray.splice(index, 1);
+    this.save(cb); 
+    }
+    /*Threads.findOneAndUpdate({_id: comment}, {$pop: {likesArray: "x"}}, function (err, db_result) {
+        if (err) return ('Unlike failed')
         return cb(null, db_result)
-    })
+    })*/
 }
 
 var Threads = mongoose.model('Threads', ThreadsSchema);
